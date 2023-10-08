a0161d3756f43e438274e0fcda568e1e5b4c49bc
Maxim Gura <maxim.gura@plus4u.net>
2023-10-08 19:17:38 +0200

    hueta

--- src/App.java
+++ src/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Print some word: ");
         String hueta = scanner.next();
 
         System.out.println(hueta);
